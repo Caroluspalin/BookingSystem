1502d724f3d36785f73ca7260e7bd883729f5cda
Carolus <caroluspalin@icloud.com>
2025-12-19 15:13:36 +0200

    chatbots data added

---
+++ chatData.js
@@ -0,0 +1,27 @@
+const chatData = [
+    {
+        id: 1,
+        question: "Do I need an appointment?",
+        answer: "We recommend booking to secure your spot, but we do accept walk-ins if we have free chairs available!"
+    },
+    {
+        id: 2,
+        question: "Where can I park?",
+        answer: "There is street parking on Erottajankatu and a large parking garage (Q-Park) just around the corner."
+    },
+    {
+        id: 3,
+        question: "Do you cut long hair?",
+        answer: "Absolutely. Our barbers are trained in both classic short cuts and longer scissor styles."
+    },
+    {
+        id: 4,
+        question: "Price list?",
+        answer: "A standard haircut is 45€. You can see the full price list in the <a href='#services' style='text-decoration:underline; font-weight:bold;'>Services section</a>."
+    },
+    {
+        id: 5,
+        question: "Cancel appointment?",
+        answer: "Please call us at +358 40 123 4567 at least 24h in advance to cancel or reschedule."
+    }
+];
